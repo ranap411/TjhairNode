eb7ca33dd327f78ad01bcd42a6bd72921e5c02fd
Prashant Rana <prashant.rana@lendingkart.com>
2019-10-17 18:23:38 +0530

    Third commit

--- app.js
+++ app.js
@@ -38,7 +38,7 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
-var port = process.env.PORT || 3000;
+var port = process.env.PORT || 4000;
 app.listen(port , ()=>{
   console.log('server is running on : ',port);
 });
