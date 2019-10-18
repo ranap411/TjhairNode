91314aea748e5e6be740fb5cc0776c6f927bc19a
Prashant Rana <prashant.rana@lendingkart.com>
2019-10-18 12:01:29 +0530

    Mongo

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,20 @@
 var express = require('express');
 var router = express.Router();
+var mongo = require('mongodb').MongoClient;
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  MongoClient.connect("mongodb://localhost:27017/test", function(err, db) {
+    if (err) next
+    db
+      .collection("users")
+      .find()
+      .toArray(function(err, result) {
+        if (err) throw err;
+
+        res.json(result)
+      });
+  });
 });
 
 module.exports = router;
