2108baeff2cc370f1c0d61aef9d382b67f09f612
Prashant Rana <prashant.rana@lendingkart.com>
2019-10-18 21:00:06 +0530

    Changing PORT

--- app.js
+++ app.js
@@ -39,7 +39,7 @@ app.use(function(err, req, res, next) {
 });
 
 var port = process.env.PORT||8080;
-app.listen(port , ()=>{
+app.listen(port ,'0.0.0.0', ()=>{
   console.log('server is running on the PORT :',port);
 });
 
