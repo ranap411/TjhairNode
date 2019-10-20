86d69053084de8beb39ef2551b33d04d3c7f0148
Prashant Rana <prashant.rana@lendingkart.com>
2019-10-20 10:41:56 +0530

    Mongo

--- routes/index.js
+++ routes/index.js
@@ -5,9 +5,11 @@ var mongo = require('mongodb').MongoClient;
 /* GET home page. */
 router.get('/', function(req, res, next) {
   console.log('server get called : ');
-  mongo.connect("mongodb://localhost:27017/test", function(err, db) {
-    if (err) next
-    res.send('Connecting to db');
+  mongo.connect("mongodb://tjhair:ranap411@docdb-2019-10-20-04-40-24.cluster-czbjrjivzoox.ap-south-1.docdb.amazonaws.com:27017/?ssl=true&ssl_ca_certs=rds-combined-ca-bundle.pem&replicaSet=rs0",{ useUnifiedTopology: true }, function(err, db) {
+    if (err) throw err
+
+    console.log('Server DB : ',db);
+    res.send('coonect db : ',db);
   });
 });
 
