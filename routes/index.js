640338425b2fb7ef2c230aafb8d98c289b4e8fd7
Prashant Rana <prashant.rana@lendingkart.com>
2019-10-20 17:40:09 +0530

    Change

--- routes/index.js
+++ routes/index.js
@@ -8,11 +8,8 @@ router.get('/', function(req, res, next) {
   const uri = 'mongodb://tjhair:ranap411@docdb-2019-10-20-04-40-24.cluster-czbjrjivzoox.ap-south-1.docdb.amazonaws.com:27017/test?ssl=true&ssl_ca_certs=rds-combined-ca-bundle.pem&replicaSet=rs0';
   mongo.connect(uri,{useNewUrlParser: true},(err,db)=>{
     if(err) res.status(500).send(err);
-
-    var collection = db.collection('col');
-    var data = collection.find({});
-    console.log(data);
-    res.send();
+    console.log('connection to db');
+    res.send('connecting to DB');
   });
 });
 
