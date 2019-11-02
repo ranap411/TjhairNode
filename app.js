3edc1f122572313a5c4fec0e6d98b871c01e1e93
Prashant Rana <prashant.rana@lendingkart.com>
2019-11-02 11:13:58 +0530

    USER module

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-var connection = require('./routes/dbConnection');
+var connection = require('./routes/dbConnection');//DB connection
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,21 @@
 var express = require('express');
 var router = express.Router();
+const connection = require('./dbConnection');
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.post('/', function(req, res, next) {
+  console.log('Server gets called :',req.body);
+  connection.get().collection('Tjhair_Users').findOne(req.body)
+  .then((result)=>{
+    console.log('request from data base : '+result);
+    if(result !=null){
+      res.json(result);
+    }else{
+      res.status(412).send({"status":"failed","message":"user not found"});
+    }
+  }).catch((error)=>{
+    res.status(500).send(error);
+  })
 });
 
 module.exports = router;
