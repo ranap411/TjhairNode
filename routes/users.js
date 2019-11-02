03fecd4965e37cb849802b30e5597823dcdf6173
Prashant Rana <prashant.rana@lendingkart.com>
2019-11-02 19:03:11 +0530

    USER module

--- routes/users.js
+++ routes/users.js
@@ -4,13 +4,14 @@ const connection = require('./dbConnection');
 
 /* GET users listing. */
 router.post('/', function(req, res, next) {
-  console.log('Server gets called :',req.body);
+  console.log(`Server gets called ${req.baseUrl} with request => : ${req.body}`);
   connection.get().collection('Tjhair_Users').findOne(req.body)
   .then((result)=>{
     console.log('request from data base : '+JSON.stringify(result));
     if(result !=null){
       res.json(result);
     }else{
+      console.log('No response from database : ');
       res.status(412).send({"status":"failed","message":"user not found"});
     }
   }).catch((error)=>{
